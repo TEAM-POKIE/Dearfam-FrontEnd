a645f500e44366fe5888502bbe2fd2d38851c3de
kimjunha1231 <tto9011@naver.com>
2025-03-13 15:30:40 +0900

    feat: 버튼 세팅 완료

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
+import CommonButton from "@/components/common_btn";
+
 export function HomePage() {
   return (
     <div className="container mx-auto py-8">
       <h1 className="text-3xl font-bold mb-6">홈</h1>
+      <div className="flex items-center justify-center">
+        <CommonButton text="테스트sd" color="main_2" size={270} />
+      </div>
     </div>
   );
 }
