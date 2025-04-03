5334b34078c7fe83261c368fc0cacc818e047be4
kimjunha1231 <tto9011@naver.com>
2025-04-03 17:09:25 +0900

    feat: BasicToastMessage완성

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import { BasicInputBox } from "../components/ui/section1/BasicInputBox";
 import { BasicDropDown } from "../components/ui/section1/BasicDropDown";
 import { Button } from "../components/ui/button";
 import { useNavigate } from "react-router-dom";
-
+import { BasicToast } from "../components/BasicToast";
 export function HomePage() {
   // 팝업 표시 여부를 관리하는 상태 추가
   const [isPopupOpen, setIsPopupOpen] = React.useState(false);
@@ -20,18 +20,19 @@ export function HomePage() {
       <div className="flex flex-col items-center justify-center gap-4 w-full">
         <BasicInputBox placeholder="text" />
         <BasicAlert message="텍스트를 입력해주세요" />
-        <BasicDropDown 
-          value={dropdownValue} 
-          onChange={setDropdownValue} 
-          options={dropdownOptions} 
+        <BasicDropDown
+          value={dropdownValue}
+          onChange={setDropdownValue}
+          options={dropdownOptions}
         />
-        <Button 
+        <Button
           variant="default"
           className="bg-blue-500 text-white"
-          onClick={() => navigate('/StartPage')}
+          onClick={() => navigate("/StartPage")}
         >
           시작 페이지로 이동
         </Button>
+        <BasicToast message="토스트 메세지 내용을 넣어주세요." />
       </div>
     </div>
   );
