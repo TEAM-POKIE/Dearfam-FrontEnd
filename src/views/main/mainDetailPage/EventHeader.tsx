e9530e507817ff6ef4eac4e1e514b189c8195739
kimjunha1231 <tto9011@naver.com>
2025-04-15 16:27:15 +0900

    feat: 디테일 페이지 완성 더보기 설정 버튼 수정완료

--- src/views/main/mainDetailPage/EventHeader.tsx
+++ src/views/main/mainDetailPage/EventHeader.tsx
@@ -1,5 +1,11 @@
 import iconImage from "../../../assets/image/style_icon_profile.svg";
 import etcIcon from "../../../assets/image/section2/icon_etc.svg";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 export const EventHeader = () => {
   return (
     <div className="flex items-center justify-between px-[1.25rem] h-[3.125rem] ">
@@ -11,7 +17,21 @@ export const EventHeader = () => {
         />
         <div>EventWriter</div>
       </div>
-      <img src={etcIcon} alt="설정" />
+      <DropdownMenu>
+        <div className="outline-none ring-0 focus:outline-none focus-visible:outline-none focus:ring-0">
+          <DropdownMenuTrigger>
+            <img
+              src={etcIcon}
+              alt="더보기"
+              className="w-[1rem] h-[1rem] hover:outline-none focus:outline-none focus-visible:outline-none"
+            />
+          </DropdownMenuTrigger>
+        </div>
+        <DropdownMenuContent>
+          <DropdownMenuItem>삭제하기</DropdownMenuItem>
+          <DropdownMenuItem>수정하기</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
