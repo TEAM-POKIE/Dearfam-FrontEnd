a7a7e785d6a4f073cd2703f65cd77dc53340a73e
kimjunha1231 <tto9011@naver.com>
2025-07-10 15:47:21 +0900

    feat: 모바일 fixed 문제 해결

--- src/pages/Home/MemoryDetailPage.tsx
+++ src/pages/Home/MemoryDetailPage.tsx
@@ -8,17 +8,19 @@ import { ImageSlider } from "./mainDetailPage/ImageSlider";
 
 export function MemoryDetailPage() {
   return (
-    <div>
-      <SemiHeader title="이벤트 제목" exit={false} />
-      <EventHeader />
-      <ImageSlider />
-      <DetailContentHeader />
-      <DetailContent />
-      <div className=" border-t-[0.0625rem] border-gray-3 ">
-        <CommentContainer />
+    <div className="min-h-screen flex flex-col w-full">
+      <div className="flex-1 pb-[4.125rem] overflow-y-auto">
+        <SemiHeader title="이벤트 제목" exit={false} />
+        <EventHeader />
+        <ImageSlider />
+        <DetailContentHeader />
+        <DetailContent />
+        <div className=" border-t-[0.0625rem] border-gray-3 ">
+          <CommentContainer />
+        </div>
       </div>
 
-      <div className="fixed w-full bottom-0 ">
+      <div className="fixed left-0 right-0 bottom-0 w-full sm:w-[24.375rem] m-auto z-10">
         <InputContainer />
       </div>
     </div>
