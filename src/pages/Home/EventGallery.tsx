19d05303d82a20d7d9b42b8994bddc07549e44d5
kimjunha1231 <tto9011@naver.com>
2025-07-14 13:24:59 +0900

    fix: 충돌 해결

--- src/pages/Home/EventGallery.tsx
+++ src/pages/Home/EventGallery.tsx
@@ -4,7 +4,7 @@ import { ImageWithProfiles } from "./components/ImageWithProfiles";
 import { useMemoryPostsByTimeOrder } from "../../hooks/api";
 import { Skeleton } from "../../components/ui/shadcn/skeleton";
 
-export default function EventGallery() {
+function EventGallery() {
   const { data, isLoading, error } = useMemoryPostsByTimeOrder({
     page: 1,
     limit: 50, // 충분한 데이터를 가져와서 연도별 그룹화
@@ -179,3 +179,5 @@ export default function EventGallery() {
     </div>
   );
 }
+
+export default EventGallery;
